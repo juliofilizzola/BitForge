14a0f7ac440edbdbc3256f0a3a91b82e47e04dc9
Julio Filizzola <juliofilizzola@hotmail.com>
2025-04-17 13:06:30 -0300

    Refactor headers and improve CLI code formatting
    Updated include paths for clarity and consistency across the project. Refactored the CLI main function to improve readability by adjusting formatting and code structure.

--- src/cli/main.c
+++ src/cli/main.c
@@ -2,13 +2,15 @@
 // Created by julio-filizzola on 17/04/25.
 //
 
-#include "../include/cli/main.h"
-#include "../include/commands/init.h"
+#include "include/cli/main.h"
+#include "include/commands/init.h"
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-  if (argc < 2) {
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
         printf("Uso: bitforge <comando> [args]\n");
         printf("Comandos disponíveis:\n");
         printf("  init       Inicializa um novo repositório\n");
@@ -17,8 +19,9 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    if (strcmp(argv[1], "init") == 0) {
+    if (strcmp(argv[1], "init") == 0)
+    {
         return init_bit_forge();
     }
-
+    return 0;
 }
