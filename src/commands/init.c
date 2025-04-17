417defeb41222244e4d92a2963c02c4d6a2d7b3c
Julio Filizzola <juliofilizzola@hotmail.com>
2025-04-17 13:06:22 -0300

    Refactor `init_bit_forge` to enhance readability and structure
    Updated code formatting for consistent style and readability, including braces positioning and spacing. Reorganized includes and removed unused paths to avoid redundancy. These changes improve maintainability without altering functionality.

--- src/commands/init.c
+++ src/commands/init.c
@@ -1,17 +1,18 @@
 //
 // Created by julio-filizzola on 17/04/25.
 //
-#include <stdlib.h>
+#include "include/commands/init.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
-#include "../include/commands/init.h"
 
-
-int init_bit_forge() {
+int init_bit_forge()
+{
     int status;
     status = mkdir(".bitForge", 0755);
-    if (status != 0) {
+    if (status != 0)
+    {
         perror("Error creating directory .bitForge");
         return 1;
     }
@@ -19,13 +20,15 @@ int init_bit_forge() {
     status = mkdir(".bitForge/objects", 0755);
     status |= mkdir(".bitForge/refs", 0755);
 
-    if (status != 0) {
+    if (status != 0)
+    {
         perror("Error creating directory objects or refs");
         return 1;
     }
 
     FILE *head = fopen(".git/HEAD", "w");
-    if (!head) {
+    if (!head)
+    {
         perror("Erro ao criar arquivo .git/HEAD");
         return 1;
     }
@@ -33,21 +36,19 @@ int init_bit_forge() {
     fputs("ref: refs/heads/main\n", head);
     fclose(head);
 
-
- 	FILE *config = fopen(".git/config", "w");
-    if (!config) {
+    FILE *config = fopen(".git/config", "w");
+    if (!config)
+    {
         perror("Erro ao criar .git/config");
         return 1;
     }
-    fprintf(config,
-        "[core]\n"
-        "\trepositoryformatversion = 0\n"
-        "\tfilemode = true\n"
-        "\tbare = false\n"
-        "[user]\n"
-        "\tname = Default User\n"
-        "\temail = user@example.com\n"
-    );
+    fprintf(config, "[core]\n"
+                    "\trepositoryformatversion = 0\n"
+                    "\tfilemode = true\n"
+                    "\tbare = false\n"
+                    "[user]\n"
+                    "\tname = Default User\n"
+                    "\temail = user@example.com\n");
     fclose(config);
 
     printf("Repositório BitForge inicializado com sucesso!\n");
