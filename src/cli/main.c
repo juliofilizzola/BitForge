140b664f02e1409b22cb8da977488b7f24baa1f5
Julio Filizzola <juliofilizzola@hotmail.com>
2025-04-24 12:58:46 -0300

    Add command handling and error codes to CLI main logic
    Introduced constants, enums, and error handling for better command processing in the CLI. Added support for 'init,' 'config,' and 'help' commands with global subcommand handling. Improved error messages and ensured unknown commands are handled gracefully.

--- src/cli/main.c
+++ src/cli/main.c
@@ -7,6 +7,21 @@
 #include <stdio.h>
 #include <string.h>
 
+const char* CMD_INIT = "init";
+const char* CMD_CONFIG = "config";
+const char* SUBCMD_GLOBAL = "global";
+const char* CMD_HELP = "help";
+
+enum ReturnCode {
+    SUCCESS = 0,
+    ERROR_INVALID_ARGS = 1,
+    ERROR_CONFIG_EXISTS = 2
+};
+
+void print_error(const char* message) {
+    fprintf(stderr, "Erro: %s\n", message);
+}
+
 void print_usage()
 {
     printf("Uso: bitforge <comando> [args]\n");
@@ -14,19 +29,43 @@ void print_usage()
     printf("  init ->      Inicializa um novo repositório\n");
     printf("  add  ->     Adiciona arquivos ao repositório\n");
     printf("  commit  ->   Cria um commit\n");
+    printf("  push  ->     Envia alterações para o repositório remoto\n");
+    printf("  config  ->   Criar configuração do git\n");
 }
 
-int main(int argc, char *argv[])
-{
-    if (argc < 2)
-    {
+int main(int argc, char* argv[]) {
+
+    if (argc < 2) {
         print_usage();
-        return 1;
+        return ERROR_INVALID_ARGS;
     }
 
-    if (strcmp(argv[1], "init") == 0)
-    {
+    const char* command = argv[1];
+
+    if (strcmp(command, CMD_INIT) == 0) {
         return init_bit_forge();
     }
-    return 0;
+
+    if (strcmp(command, CMD_HELP) == 0) {
+        print_usage();
+        return SUCCESS;
+    }
+
+    if (strcmp(command, CMD_CONFIG) == 0) {
+        if (argc < 3) {
+            print_error("Uso: bitforge config <global>");
+            return ERROR_INVALID_ARGS;
+        }
+
+        if (strcmp(argv[2], SUBCMD_GLOBAL) == 0) {
+            if (config_exists()) {
+                print_error("Configuração global já existe.");
+                return ERROR_CONFIG_EXISTS;
+            }
+            return create_global_config();
+        }
+    }
+
+    print_error("Comando desconhecido");
+    return ERROR_INVALID_ARGS;
 }
