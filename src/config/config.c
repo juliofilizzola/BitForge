420d1ca6720b19407137fc15465fc597378fcf57
Julio Filizzola <juliofilizzola@hotmail.com>
2025-04-30 11:37:01 -0300

    Add function to update global configuration key-value pairs

--- src/config/config.c
+++ src/config/config.c
@@ -30,6 +30,53 @@ int config_exists()
     return access(path, F_OK) == 0;
 }
 
+
+int update_global_config(const char *key, const char *value) {
+    char path[512];
+    get_config_path(path, sizeof(path));
+
+    FILE *file = fopen(path, "r+");
+    if (!file) {
+        perror("Error opening global config file");
+        return 1;
+    }
+
+    char buffer[1024];
+    int key_found = 0;
+    FILE *temp = tmpfile();
+    if (!temp) {
+        perror("Error creating temporary file");
+        fclose(file);
+        return 1;
+    }
+
+    while (fgets(buffer, sizeof(buffer), file)) {
+        if (strncmp(buffer, key, strlen(key)) == 0 && buffer[strlen(key)] == ' ') {
+            fprintf(temp, "%s = %s\n", key, value);
+            key_found = 1;
+        } else {
+            fputs(buffer, temp);
+        }
+    }
+
+    if (!key_found) {
+        fprintf(temp, "%s = %s\n", key, value);
+    }
+
+    rewind(file);
+    rewind(temp);
+
+    while (fgets(buffer, sizeof(buffer), temp)) {
+        fputs(buffer, file);
+    }
+
+    fclose(file);
+    fclose(temp);
+
+    return 0;
+}
+
+
 int create_global_config() {
     char path[512];
     get_config_path(path, sizeof(path));
